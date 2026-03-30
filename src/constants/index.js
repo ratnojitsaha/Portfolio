f8c8982d2dfc8bdce2a544b51d5c307b902c778f
Ratnojit Saha <saharatnojit04@gmail.com>
2026-03-31 01:00:20 +0530

    Portfolio Updated
    Sry mama for keeping you late..

---
+++ src/constants/index.js
@@ -0,0 +1,328 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Ride-On | Vehicle Rental Platform",
+    description:
+      "Full-Stack Vehicle Rental Platform, allowing customers to rent vehicle from this site for different cities and can be useful for a vehicle rental franchise.",
+    subDescription: [
+      "Built and deployed a resilient full-stack architecture with primary_failover backend services and cloud-hosted frontend, designed for fault tolerance rather than basic availability. ",
+      "Implemented request routing and load balancing using Cloudflare Workers, enabling backend failover, centralized CORS security, transparent recovery during outages, and cost-effective high availability. ",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added payment systems, localization, and product filtering for functionality improvements.",
+    ],
+    href: "https://ride-on-eight.vercel.app/",
+    logo: "",
+    image: "/assets/projects/accessories.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Node.js",
+        path: "/assets/logos/node.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/express.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongo.svg",
+      },
+      {
+        id: 4,
+        name: "React.js",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+     title: "FOREVER | E-commerce Platform",
+    description:
+      "Facilitates purchases from international clothing brands, allowing customers to shop from them and have products delivered domestically.",
+    subDescription: [
+      "Built a scalable application with MERN Core MVC, global platforms like Myntra for domestic delivery.",
+      "Implemented secure authentication and database management using ASP.NET Core Identity and Entity Framework Core.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added payment systems, localization, and product filtering for functionality improvements.",
+    ],
+    href: "https://forever-ratno.vercel.app/",
+    logo: "",
+    image: "/assets/projects/auth-system.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Node.js",
+        path: "/assets/logos/node.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/express.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongo.svg",
+      },
+      {
+        id: 4,
+        name: "React.js",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+        title: "Vroom-Go | Vehicle Booking Platform",
+    description:
+      "Full-Stack Vehicle Rental Platform, allowing customers to book vehicle from this site for different cities like Rapido",
+    subDescription: [
+      "Built and deployed a resilient full-stack architecture with primary_failover backend services and cloud-hosted frontend, designed for fault tolerance rather than basic availability. ",
+      "Implemented request routing and load balancing using Cloudflare Workers, enabling backend failover, centralized CORS security, transparent recovery during outages, and cost-effective high availability. ",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added payment systems, localization, and product filtering for functionality improvements.",
+    ],
+    href: "https://github.com/ratnojitsaha/VroomGo",
+    logo: "",
+    image: "/assets/projects/accessories.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Node.js",
+        path: "/assets/logos/node.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/express.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongo.svg",
+      },
+      {
+        id: 4,
+        name: "React.js",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "LMS_MERN",
+    description:
+      "A Learning Management System.",
+    subDescription: [
+      "Built a powerful system with MERN Stack.",
+      "Developed a physics engine with collision detection and particle effects.",
+      "Implemented a scripting system for easy game customization.",
+      "Optimized performance with multi-threading and efficient memory management.",
+    ],
+    href: "https://github.com/ratnojitsaha/LMS_MERN",
+    logo: "",
+    image: "/assets/projects/game-engine.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Node.js",
+        path: "/assets/logos/node.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/express.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongo.svg",
+      },
+      {
+        id: 4,
+        name: "React.js",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "PayMERN",
+    description:
+      "This is a clone of Paytm, a popular digital wallet and payment system, built using the MERN stack (MongoDB, Express, React, Node.js",
+    subDescription: [
+      "It includes core functionalities such as user authentication, and transaction management.",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Optimized SEO and page speed using Vite.js for fast builds.",
+      "Implemented custom widgets and plugin compatibility for extended functionality.",
+    ],
+    href: "https://github.com/ratnojitsaha/payMERN",
+    logo: "",
+    image: "/assets/projects/wordpress-theme.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Node.js",
+        path: "/assets/logos/node.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/express.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongo.svg",
+      },
+      {
+        id: 4,
+        name: "React.js",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "QuickKart",
+    description:
+      "It is a Flipkart clone , an ecommerce website with full functional api calls and database access. It is made with pure user experience and control at top.",
+    subDescription: [
+      "It includes core functionalities such as user authentication, and transaction management.",
+      "Integrated Tailwind CSS for modern styling and UI enhancements.",
+      "Optimized SEO and page speed using Vite.js for fast builds.",
+      "Implemented custom widgets and plugin compatibility for extended functionality.",
+    ],
+    href: "https://github.com/ratnojitsaha/QuickKart",
+    logo: "",
+    image: "/assets/projects/elearning.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Node.js",
+        path: "/assets/logos/node.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/express.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongo.svg",
+      },
+      {
+        id: 4,
+        name: "React.js",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/saharatnojit/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/rat_no_jit?igsh=MWtnZzd3eTBpam1rZA==",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "#12, JWOC 2024",
+    job: "Open Source Contributor",
+    date: "feb-march, 2024",
+    contents: [
+      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
+      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
+      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
+      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
+      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
+    ],
+  },
+  // {
+  //   title: "Tech Lead",
+  //   job: "Car Manufacture",
+  //   date: "2023-2024",
+  //   contents: [
+  //     "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
+  //     "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
+  //     "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
+  //     "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
+  //     "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+  //   ],
+  // },
+  // {
+  //   title: "Freelance Developer",
+  //   job: "Self-Employed",
+  //   date: "2025-Present",
+  //   contents: [
+  //     "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+  //     "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+  //   ],
+  // },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
